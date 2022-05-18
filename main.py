a0d9581522f7e61e84b5490d7b9285a001a1ae95
PlatonovAV <noyon98@yandex.ru>
2022-05-18 11:00:58 +0300

    main scenario

---
+++ main.py
@@ -0,0 +1,52 @@
+
+from clear_dataset import clear_data as step_1
+from classification_algorithms import k_neighbors, decision_tree, naive_bayes_bernoulli, naive_bayes_gaussian, \
+    nb_compare, bagging, random_forest, ada_boost, gradient_boost
+from feature_extration import bag_of_words_vizualizer as step_3
+from unused_data_delete import unused_data_delete as step_2
+
+if __name__ == '__main__':
+    print("Первичная очистка текстовых данных начата")
+    step_1()
+    print("Очистка завершена\n")
+
+    print("Удаление неиспользуемых текстов из датасета начата")
+    step_2()
+    print("Удаление завершено\n")
+
+    print("Визуализация текстовых данных")
+    step_3()
+    print("Конец визуализации\n")
+
+    print("Классификация методом k-ближайших соседей")
+    k_neighbors()
+    print("Классификация завершена\n")
+
+    print("Классификация методом Деревьев решений")
+    decision_tree()
+    print("Классификация завершена\n")
+
+    print("Классификация методом наивного Байеса")
+    naive_bayes_bernoulli()
+    naive_bayes_gaussian()
+    nb_compare()
+    print("Классификация завершена\n")
+
+    print("Классификация с использованием бутстрапирования")
+    bagging()
+    print("Классификация завершена\n")
+
+    print("Классификация методом Деревьев решений")
+    random_forest()
+    print("Классификация завершена\n")
+
+    print("Классификация с использованием адаптивного бустинга")
+    ada_boost()
+    print("Классификация завершена\n")
+
+    print("Классификация с использованием градиентного бустинга")
+    gradient_boost()
+    print("Классификация завершена")
+
+    print("Классификация с использованием стекинга")
+    print("Классификация завершена\n")
